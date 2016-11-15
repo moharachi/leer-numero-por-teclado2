0cd7cdf85071e30010faf20e4e9919fcd7be7d5d
mohamed rachidi <mohamed rachidi>
2016-11-15 18:08:24 +0100

    prueba

---
+++ 2-nemeros-teclado.py
@@ -0,0 +1,8 @@
+# Completa el ejercicio aquí
+n1 = float( input("Introduce el primer número: ") )
+n2 = float( input("Introduce el segundo número: ") )
+
+print("¿Son iguales? ", n1 == n2)
+print("¿Son diferentes?", n1 != n2)
+print("¿El primero es mayor que el segundo?", n1 > n2)
+print("¿El segundo es mayor o igual que el primero?", n1 <= n2)
